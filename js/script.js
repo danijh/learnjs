8a2de94735931dd336c4eb90106212a6dcf8f807
Dani JH <dani_4uvi@yahoo.com>
2022-06-26 17:22:33 +0800

    menambahkan function tambahPenumpang di file script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
-var penumpang = [];
+var penumpang = ["JH", "Dani", undefined, "Juanda"];
 var tambahPenumpang = function (namaPenumpang, penumpang) {
   //jika angkot kosong
   if (penumpang.length == 0) {
-    penumpang.unshift(namaPenumpang); // tambah penumpang di awal array
-    console.log(penumpang); // kembalikan isi array & keluar dari function
+    // tambah penumpang di awal array
+    penumpang.unshift(namaPenumpang);
+    // kembalikan isi array & keluar dari function
+    return penumpang;
+    //else
+  } else {
+    // telusuri seluruh kursi dari awal
+    for (var i = 0; i < penumpang.length; i++) {
+      // jika ada kursi kosong
+      if (penumpang[i] == undefined) {
+        // tambah penumpang di kursi tersebut
+        penumpang[i] = namaPenumpang;
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+        // jika sudah ada nama yang sama
+      } else if (penumpang[i] == namaPenumpang) {
+        // tampilkan pesan kesalahannya
+        console.log(namaPenumpang + " sudah ada di dalam angkot!");
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+        // jika seluruh kursi terisi
+      } else if (i == penumpang.length) {
+        // tambah penumpang di akhir array
+        penumpang.push(namaPenumpang);
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      }
+    }
   }
-  //else
-
-  // telusuri seluruh kursi dari awal
-  // jika ada kursi kosong
-  // tambah penumpang di kursi tersebut
-
-  // kembalikan isi array & keluar dari function
-
-  // jika sudah ada nama yang sama
-
-  // tampilkan pesan kesalahannya
-  // kembalikan isi array & keluar dari function
-  // jika seluruh kursi terisi
-  // tambah penumpang di akhir array
-  // kembalikan isi array & keluar dari function
 };
 
 // tambahPenumpang("Dani", penumpang);
