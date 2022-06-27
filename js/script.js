d13e302f52e410e41fa710ed5701ec034cf10795
Dani JH <dani_4uvi@yahoo.com>
2022-06-27 14:00:42 +0800

    menambahkan coding hapusPenumpang di file script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// var penumpang = ["Dani", "JH", "Hairudin"];
 var penumpang = [];
 var tambahPenumpang = function (namaPenumpang, penumpang) {
   //jika angkot kosong
@@ -9,25 +10,32 @@ var tambahPenumpang = function (namaPenumpang, penumpang) {
     //else
   } else {
     // telusuri seluruh kursi dari awal
-    for (var i = 0; i < penumpang.length; i++) {
+    //
+    for (var i = 0; i <= penumpang.length; i++) {
       // jika ada kursi kosong
-      if (penumpang[i] == undefined) {
+      // var penumpangLama = penumpang.find(function (e) {
+      //   return e == penumpang[i];
+      // });
+      if (penumpang[i] === namaPenumpang) {
         // tambah penumpang di kursi tersebut
-        penumpang[i] = namaPenumpang;
+        console.log(namaPenumpang + " sudah ada di dalam angkot!");
+        // console.log(penumpang.join());
         // kembalikan isi array & keluar dari function
         return penumpang;
         // jika sudah ada nama yang sama
-      } else if (penumpang[i] == namaPenumpang) {
-        // tampilkan pesan kesalahannya
-        console.log(namaPenumpang + " sudah ada di dalam angkot!");
-        // kembalikan isi array & keluar dari function
-        return penumpang;
-        // jika seluruh kursi terisi
-      } else if (i == penumpang.length - 1) {
-        // tambah penumpang di akhir array
-        penumpang.push(namaPenumpang);
-        // kembalikan isi array & keluar dari function
-        return penumpang;
+      } else {
+        if (penumpang[i] == undefined) {
+          // tampilkan pesan kesalahannya
+          penumpang[i] = namaPenumpang;
+          // kembalikan isi array & keluar dari function
+          return penumpang;
+          // jika seluruh kursi terisi
+        } else if (i == penumpang.length - 1) {
+          // tambah penumpang di akhir array
+          penumpang.push(namaPenumpang);
+          // kembalikan isi array & keluar dari function
+          return penumpang;
+        }
       }
     }
   }
@@ -35,16 +43,30 @@ var tambahPenumpang = function (namaPenumpang, penumpang) {
 
 var hapusPenumpang = function (namaPenumpang, penumpang) {
   // jika angkot kosong
-  // tampilkan pesan bahwa angkot kosong, dan
-  // tidak mungkin ada penumpang turun
-  // kembalikan isi array & keluar dari function
-  // else
-  // telusuri seluruh kursi dari awal
-  // jika ada nama penumpang sesuai
-  // hapus penumpang dengan mengubah
-  // namanya menjadi undefined
-  // kembalikan isi array & keluar dari function
-  // jika tidak ada nama yg sesuai
-  // tampilkan pesan kesalahannya
-  // kembalikan isi array & keluar dari function
+  if (penumpang.length == 0) {
+    // tampilkan pesan bahwa angkot kosong, dan
+    console.log("Angkot masih kosong, tidak ada penumpang yg turun!");
+    // tidak mungkin ada penumpang turun
+    // kembalikan isi array & keluar dari function
+    return penumpang;
+    // else
+  } else {
+    // telusuri seluruh kursi dari awal
+    for (var h = 0; h <= penumpang.length; h++) {
+      // jika ada nama penumpang sesuai
+      if (penumpang[h] == namaPenumpang) {
+        // hapus penumpang dengan mengubah
+        penumpang[h] = undefined;
+        // namanya menjadi undefined
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      } else {
+        // jika tidak ada nama yg sesuai
+        console.log(namaPenumpang + " tidak ada dalam angkot!");
+        // tampilkan pesan kesalahannya
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      }
+    }
+  }
 };
